ccd0f69bd36e8580a6147118121875c9dc1d3478
Francisco Pantoja <fcopantoja@gmail.com>
2026-04-01 13:07:17 -0600

    Update: sql-customers-bought-a-b-not-c - submission-0

--- SQL/sql-customers-bought-a-b-not-c/submission-0.sql
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -1,9 +1,14 @@
 -- Write your query below
-SELECT
-    employee_id,
-    CASE
-        WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary
-        ELSE 0
-    END AS bonus
-    FROM employees
-    ORDER BY employee_id;
+
+SELECT c.customer_id, c.customer_name
+FROM customers c
+WHERE c.customer_id IN(
+    SELECT customer_id FROM orders WHERE product_name = 'A'
+)
+AND c.customer_id IN(
+    SELECT customer_id FROM orders WHERE product_name = 'B'
+)
+AND c.customer_id NOT IN(
+    SELECT customer_id FROM orders WHERE product_name = 'C'
+)
+ORDER BY c.customer_name;
