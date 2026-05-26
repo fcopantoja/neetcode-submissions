6c215bb853c6003362140d84d37cef6f8bdf0a78
Francisco Pantoja <fcopantoja@gmail.com>
2026-05-25 18:29:27 -0600

    Add: combination-target-sum-ii - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-6.py
@@ -0,0 +1,79 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        nums = candidates
+        nums.sort() # We sort because line 23 does a whileloop and scan sequentially until new number comes in
+        result = []
+
+        def backtracking(i, total, path):
+            if total == target:
+                result.append(path.copy())
+                return
+            
+            if total > target or i >= len(nums):
+                return
+            
+            path.append(nums[i])
+            backtracking(i + 1, total + nums[i], path)
+            path.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+
+            backtracking(i + 1, total, path)
+            
+        backtracking(0, 0, [])
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        nums = candidates
+        candidates.sort()
+
+        def backtracking(i, total, path):
+            if total == target:
+                result.append(path.copy())
+                return
+            
+            if total > target or i >= len(nums):
+                return
+
+            path.append(nums[i])
+            backtracking(i + 1, total + nums[i], path)
+            path.pop()
+
+            while i < (len(nums) - 1) and nums[i] == nums[i + 1]:
+                i += 1
+
+            
+            backtracking(i + 1, total, path)
+    
+        backtracking(0, 0, [])
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
