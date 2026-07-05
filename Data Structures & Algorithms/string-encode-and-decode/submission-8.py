4ffcdf315555774d8144be86c91404fbce58064d
Francisco Pantoja <fcopantoja@gmail.com>
2026-07-05 12:09:39 -0600

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for word in strs:
+            res += f"{len(word)}#{word}"
+        
+        return res
+       
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        print(s)
+        i = 0
+    
+        while i < len(s):
+            length = ""
+            while s[i].isdigit():
+                length += s[i]
+                i += 1
+            
+            i += 1
+            result.append(s[i:i + int(length)])
+            i = i + int(length)
+        
+        return result
+
+            
+        
+
+      
